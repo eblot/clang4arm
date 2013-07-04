1c5a691a82672a9fea225afd3318aba1892bde45
Emmanuel Blot <emmanuel.blot@free.fr>
2013-07-04 15:38:09 +0200

    Improve (a lot) the build script. Fix many configuration issues

--- build.sh
+++ build.sh
@@ -6,12 +6,121 @@ PREFIX="/usr/local/clang-${CLANG_VERSION}"
 BUILD="build"
 VERBOSE=0
 
+UNAME=`uname`
+if [ "${UNAME}" = "Darwin" ]; then
+    # Discard any non-system library
+    export DYLD_LIBRARY_PATH=""
+    export DYLD_FALLBACK_LIBRARY_PATH=""
+    CPUCORES=`sysctl hw.ncpu | cut -d: -f2`
+    _DEBUGGER="lldb --"
+else
+    export LD_LIBRARY_PATH=""
+    CPUCORES=1 # to be implemented on Linux
+    _DEBUGGER="gdb --args"
+fi
+
+JOBS=`expr ${CPUCORES} + 1`
+
+# Show usage information
+usage()
+{
+    NAME=`basename $0`
+    cat <<EOT
+$NAME [options] [args]
+  Build and execute Neotion VM
+    -h              Print this help message
+    -c              Clean all
+    -j N            Build jobs (default: ${JOBS})
+    -p path         Installation path (default: ${PREFIX})
+    -t toolchain    Toolchain prefix (default: ${XTOOLCHAIN})
+
+  Debug mode
+    -l              Run clang build/installation stage
+    -w              Run wrapper build/installation stage
+    -n              Run newlib build/installation stage
+    -r              Run compiler runtime build/installation stage
+EOT
+}
+
+CLEAN=0
+VERBOSE=0
+RUN_CLANG=0
+RUN_NEWLIB=0
+RUN_RUNTIME=0
+RUN_WRAPPER=0
+RUN_ALL=1
+
+# Parse the command line and update configuration
+while [ $# -gt 0 ]; do
+    case "$1" in
+      -h)
+        usage
+        exit 0
+        ;;
+      -c)
+        CLEAN=1
+        ;;
+      -j*)
+        JOBS=`echo "$1" | cut -c3-`
+        if [ -n "${JOBS}" ]; then
+            shift
+            JOBS="$1"
+        fi
+        ;;
+      -v)
+        VERBOSE=1
+        ;;
+      -p)
+		shift
+		PREFIX="$1"
+		;;
+      -t)
+		shift
+		XTOOLCHAIN="$1"
+		;;
+	  -l)
+		RUN_CLANG=1
+		RUN_ALL=0
+		;;
+	  -n)
+		RUN_NEWLIB=1
+		RUN_ALL=0
+		;;
+	  -r)
+		RUN_RUNTIME=1
+		RUN_ALL=0
+		;;
+	  -w)
+		RUN_WRAPPER=1
+		RUN_ALL=0
+		;;
+      '')
+        break
+        ;;
+      *)
+        usage
+        echo ""
+        echo "Unsupported option: '$1'" >&2
+        exit 1
+        ;;
+    esac
+    shift
+done
+
+if [ ${RUN_ALL} -gt 0 ]; then
+	RUN_CLANG=1
+	RUN_NEWLIB=1
+	RUN_RUNTIME=1
+	RUN_WRAPPER=1
+fi
+
 # Verify GNU make
 MAKE=`which make 2> /dev/null`
 if [ -z "${MAKE}" ]; then
     echo "Missing or invalid GNU make tool"
     exit 1
 fi
+MAKEVER_STR=`${MAKE} --version 2>&1 | head -1 | sed s'/^[^0-9\.]*//'`
 
 ARMAS=`which ${XTOOLCHAIN}-as 2> /dev/null`
 if [ -z "${ARMAS}" ]; then
@@ -38,9 +147,13 @@ fi
 
 TOPDIR="$PWD"
 
-if [ ! -d "${PREFIX}" ]; then
-	echo "Destination directory ${PREFIX} does not exist" >&2
+if [ ${RUN_CLANG} -gt 0 -a -d "${PREFIX}" ]; then
+	echo "Destination directory ${PREFIX} already exists" >&2
 	exit 1
+else
+	echo "Creating destination directory"
+	(mkdir -p "${PREFIX}" 2> /dev/null) || \
+	(sudo mkdir -p "${PREFIX}" && sudo chown $USER "${PREFIX}") || exit 1
 fi
 
 if [ ! -d "${PREFIX}" ]; then
@@ -53,50 +166,75 @@ if [ ! -w "${PREFIX}" ]; then
 	exit 1
 fi
 
-echo "Building clang..."
-# out-of-source build
-mkdir -p "${BUILD}/llvm"
-(cd "${BUILD}/llvm" && \
-	"${TOPDIR}/llvm/configure" \
-		--prefix="${PREFIX}" && \
-	make &&
-	make install) || exit 1
-export CC="${PREFIX}/bin/clang"
-echo "---"
-echo ""
+if [ ${RUN_CLANG} -gt 0 ]; then
+	echo "Building clang..."
+	# out-of-source build
+	mkdir -p "${BUILD}/llvm"
+	(cd "${BUILD}/llvm" && \
+		"${TOPDIR}/llvm/configure" \
+			--prefix="${PREFIX}" && \
+		make -j${JOBS} &&
+		make install) || exit 1
+	export CC="${PREFIX}/bin/clang"
+	echo "---"
+	echo ""
+fi
 
 # new clang should take the precedence over any installed clang, such as 
 # Apple's
 export PATH="${PREFIX}/bin":$PATH
 
-echo "Building gcc wrapper for ${XTOOLCHAIN}..."
-(cd wrapper &&
-	clang -std=c99 -O3 -o wrapper wrapper.c &&
-	cp wrapper "${PREFIX}/bin/${XTOOLCHAIN}-gcc"} &&
-	cat wrapper.py | \
-		sed 's^XTOOLCHAIN=""^XTOOLCHAIN="'${XTOOLCHAIN}'"^' | \
-		sed 's^NEWLIB=""^NEWLIB="'${PREFIX}'"^' | \
-		sed 's^COMPILER_RT=""^COMPILER_RT="'${PREFIX}/lib/${XTOOLCHAIN}'"^' > \
-			"${PREFIX}/bin/wrapper.py")
-echo "---"
-echo ""
+if [ ${RUN_WRAPPER} -gt 0 ]; then
+	echo "Building gcc wrapper for ${XTOOLCHAIN}..."
+	mkdir -p build/wrapper
+	(cd build/wrapper &&
+		clang -std=c99 -O3 -o wrapper ../../wrapper/wrapper.c &&
+		cp wrapper "${PREFIX}/bin/${XTOOLCHAIN}-gcc" &&
+		cat ../../wrapper/wrapper.py | \
+			sed 's^XTOOLCHAIN=""^XTOOLCHAIN="'${XTOOLCHAIN}'"^' | \
+			sed 's^NEWLIB=""^NEWLIB="'${PREFIX}'"^' | \
+			sed 's^COMPILER_RT=""^COMPILER_RT="'${PREFIX}/lib/${XTOOLCHAIN}'"^' > \
+				"${PREFIX}/bin/wrapper.py") || exit 1
+	echo "---"
+	echo ""
+fi
 
-echo "Building newlib for ${XTOOLCHAIN}..."
-# out-of-source build
-mkdir -p "${BUILD}/newlib"
-(cd "${BUILD}/newlib" && \
-	"${TOPDIR}/newlib/configure" \
-		--prefix="${PREFIX}" \
-		--target="${XTOOLCHAIN}" && \
-	make &&
-	make install) || exit 1
-echo "---"
-echo ""
+if [ ! -x "${PREFIX}/bin/${XTOOLCHAIN}-gcc" ]; then
+	echo "Missing GCC wrapper" >&2
+	exit 1
+fi
 
-echo "Building compiler-rt for ${XTOOLCHAIN}..."
-# in-source build
-(cd "${TOPDIR}/llvm/projects/compiler-rt" && \
-	make XTOOLCHAIN="${XTOOLCHAIN}" NEWLIB="${PREFIX}/newlib" \
-		clang_generic_arm) || exit 1
-echo "---"
-echo ""
+if [ ${RUN_NEWLIB} -gt 0 ]; then
+	echo "Building newlib for ${XTOOLCHAIN}..."
+	# out-of-source build
+	mkdir -p "${BUILD}/newlib"
+	CLANG="${PREFIX}/bin/clang"
+	export CC="${CLANG}"
+	export CC_FOR_TARGET="${CLANG}"
+	export CXX_FOR_TARGET="${CLANG}"
+	export GCC_FOR_TARGET="${CLANG}"
+	CLFLAGS="-Qunused-arguments -O3 -v -target ${XTOOLCHAIN}"
+	CLFLAGS="${CLFLAGS} -isystem${PREFIX}/lib/clang/${CLANG_VERSION}/include"
+	CLFLAGS="${CLFLAGS} -isystem${PWD}/compiler-rt/SDKs/linux/usr/include"
+	export CFLAGS_FOR_TARGET="${CLFLAGS}"
+	export CXXFLAGS_FOR_TARGET="${CLFLAGS}"
+	(cd "${BUILD}/newlib" && \
+		"${TOPDIR}/newlib/configure" \
+		    --enable-serial-configure \
+			--prefix="${PREFIX}" \
+			--target="${XTOOLCHAIN}" && \
+		make -j${JOBS} &&
+		make install) || exit 1
+	echo "---"
+	echo ""
+fi
+
+if [ ${RUN_RUNTIME} -gt 0 ]; then
+	echo "Building compiler-rt for ${XTOOLCHAIN}..."
+	# in-source build
+	(cd "${TOPDIR}/llvm/projects/compiler-rt" && \
+		make -j${JOBS} XTOOLCHAIN="${XTOOLCHAIN}" NEWLIB="${PREFIX}/newlib" \
+			clang_generic_arm) || exit 1
+	echo "---"
+	echo ""
+fi
